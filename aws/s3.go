656e3b55b0de0ea9f3e9ed373747b15a1577bd75
Azuma Fan <azuma@theplant.jp>
2017-06-15 21:49:07 +0900

    add s3 prefix

--- aws/s3.go
+++ aws/s3.go
@@ -102,6 +102,9 @@ func (s S3) GetURLTemplate(option *media_library.Option) (path string) {
 	if path = option.Get("URL"); path == "" {
 		path = "/{{class}}/{{primary_key}}/{{column}}/{{filename_with_hash}}"
 	}
+	if awsS3PathPrefix != "" {
+		path = "/" + awsS3PathPrefix + path
+	}
 	return "//" + getEndpoint(option) + path
 }
 
